8df54510f03e818ed62e8e9855a18207aefb3acb
Kirill Ki <kirillkalapov@gmail.com>
2022-06-02 13:26:29 +0200

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by  Mr.Ki on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
